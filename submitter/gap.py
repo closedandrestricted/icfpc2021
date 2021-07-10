2f6f4b736b9a7f2dade700681774977004515b9b
Evgeniy Krasko <evgeniykrasko@MacBook-Air.local>
2021-07-10 12:12:03 -0700

    total gap

--- submitter/gap.py
+++ submitter/gap.py
@@ -9,6 +9,7 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 
 scores = pandas.read_csv("scores.csv")
 print(scores)
+total_gap = 0
 for idx, row in scores.iterrows():
     with open(os.path.join(this_dir, '../problems/%d.json' % row['problem'])) as f:
         problem = json.loads(f.read())
@@ -17,11 +18,14 @@ for idx, row in scores.iterrows():
         le = len(problem['figure']['edges'])
         lh = len(problem['hole'])
         lv = len(problem['figure']['vertices'])
-        a = 1000 * math.log2(le * lh * lv)
+        a = 1000 * math.log2(le * lh * lv / 6)
         b = math.sqrt((1. + row['best_score']) / (1. + player_score))
         if player_score != player_score:
-            return float('nan')
+            return 0
         return int(math.ceil(a * b))
 
     gap = problem_score(row['best_score']) - problem_score(row['score'])
     print(int(row['problem']), gap)
+    total_gap += gap
+
+print('TOTAL = ', total_gap)
