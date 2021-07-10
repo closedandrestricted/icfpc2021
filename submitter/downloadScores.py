f37dfff143b4d0d3b4ea9797e2264e4e69c16330
Den Raskovalov <denplusplus@yandex-team.ru>
2021-07-10 11:30:49 -0700

    add script for downloading scores

---
+++ submitter/downloadScores.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import requests
+import re
+
+with requests.Session() as session:
+    r = session.post("https://poses.live/login", data={"login.email": "denboxarchive@gmail.com", "login.password": "B0ltzMaNn"})
+    # print(r.text)
+    
+    with open("scores.csv", "w") as fOut:
+        matchTrs = re.findall(r'<tr>(.*?)</tr>', r.text)
+        for index, tr in enumerate(matchTrs):
+            # print(tr)
+            matchTds = re.findall(r'<td>(.*?)</td>', tr)
+            if len(matchTds) == 3:
+                print("%d,%s,%s" % (index, matchTds[1], matchTds[2]), file=fOut)
