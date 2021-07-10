771cf69353ea0e2d20b6f284b9a557cab70beb08
Den Raskovalov <denplusplus@yandex-team.ru>
2021-07-10 00:54:04 -0700

    cleanup

--- submitter/submitter.py
+++ submitter/submitter.py
@@ -17,6 +17,5 @@ args = parser.parse_args()
 apiKey = open("../api_key2", "r").read().strip()
 
 with open(args.solution, "r") as fIn:
-    params = {"file": fIn}
     response = requests.post("https://poses.live/api/problems/%d/solutions" % args.problem, data=fIn.read(), headers={"Authorization": "Bearer %s" % apiKey})
     print(response.text)
