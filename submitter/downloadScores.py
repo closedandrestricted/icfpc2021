3ecc3f3e1c8b3c1c486fe4b2929e13d4ca6cd62c
Evgeniy Krasko <evgeniykrasko@MacBook-Air.local>
2021-07-10 12:14:58 -0700

    download_scores invokes gap

--- submitter/downloadScores.py
+++ submitter/downloadScores.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 import requests
 import re
+import subprocess
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
 
 with requests.Session() as session:
     r = session.post("https://poses.live/login", data={"login.email": "denboxarchive@gmail.com", "login.password": "B0ltzMaNn"})
@@ -15,3 +19,5 @@ with requests.Session() as session:
             matchTds = re.findall(r'<td>(.*?)</td>', tr)
             if len(matchTds) == 3:
                 print("%d,%s,%s" % (index, matchTds[1], matchTds[2]), file=fOut)
+
+    subprocess.check_call(os.path.join(this_dir, 'gap.py'), shell=True)
