da53a01e935bfb4a76e5bfb4a4e56583329983c7
Evgeniy Krasko <evgeniykrasko@MacBook-Air.local>
2021-07-10 12:08:19 -0700

    fix formula

--- submitter/gap.py
+++ submitter/gap.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 
+import json
 import pandas
 import math
+import os
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
 
 scores = pandas.read_csv("scores.csv")
 print(scores)
 for idx, row in scores.iterrows():
-    print(idx, row['score']*math.sqrt(row['score']/row['best_score']))
+    with open(os.path.join(this_dir, '../problems/%d.json' % row['problem'])) as f:
+        problem = json.loads(f.read())
+
+    def problem_score(player_score):
+        le = len(problem['figure']['edges'])
+        lh = len(problem['hole'])
+        lv = len(problem['figure']['vertices'])
+        a = 1000 * math.log2(le * lh * lv)
+        b = math.sqrt((1. + row['best_score']) / (1. + player_score))
+        if player_score != player_score:
+            return float('nan')
+        return int(math.ceil(a * b))
+
+    gap = problem_score(row['best_score']) - problem_score(row['score'])
+    print(int(row['problem']), gap)
