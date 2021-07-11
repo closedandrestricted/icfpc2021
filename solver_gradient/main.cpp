27a21ea2a3268363ff50124e7fcbe842f6026550
Den Raskovalov <denplusplus@yandex-team.ru>
2021-07-11 14:57:44 -0700

    dummy

--- solver_gradient/main.cpp
+++ solver_gradient/main.cpp
@@ -18,6 +18,9 @@ int main(int argc, char* argv[]) {
     std::cerr << FLAGS_test_idx << " ";
     p.parseJson("../problems/" + std::to_string(FLAGS_test_idx) + ".json");
     p.preprocess(false);
+    cerr << endl;
+
+    vector<SolutionCandidate> population;
 
     std::ofstream f("../solutions/gradient/" + std::to_string(FLAGS_test_idx) + ".json");
     std::vector<int> points;
