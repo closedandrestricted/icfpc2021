d0264a92313c44fa0fb5364186ec2c112e8e202d
Den Raskovalov <denplusplus@yandex-team.ru>
2021-07-10 19:29:18 -0700

    read problem

--- solver_gradient/main.cpp
+++ solver_gradient/main.cpp
@@ -4,11 +4,20 @@
 
 #include "json.hpp"
 
+#include <gflags/gflags.h>
+
+DEFINE_int32(test_idx, 1, "Test number");
+
 using namespace std;
 
 using json = nlohmann::json;
 
+int main(int argc, char* argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    Problem p;
+    std::cerr << FLAGS_test_idx << " ";
+    p.parseJson("problems/" + std::to_string(FLAGS_test_idx) + ".json");
+    p.preprocess();
 
-int main() {
     return 0;
 }
