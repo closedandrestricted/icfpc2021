fbe120f9a2e96e729cbbd6c8f57cacdd37eee42d
Den Raskovalov <denplusplus@yandex-team.ru>
2021-07-11 08:19:30 -0700

    update

--- solutions/makeGolden.py
+++ solutions/makeGolden.py
@@ -5,10 +5,12 @@ import argparse
 import subprocess
 import shutil
 
+maxProblem = int(open("../max_problem", "r").read().strip())
+
 parser = argparse.ArgumentParser(description='Submit ICFPC 2021 solutions.')
 parser.add_argument('--begin', type=int, default=1,
                     help='first problem number')
-parser.add_argument('--end', type=int, default=106,
+parser.add_argument('--end', type=int, default=maxProblem,
                     help='last problem number')
 
 args = parser.parse_args()
