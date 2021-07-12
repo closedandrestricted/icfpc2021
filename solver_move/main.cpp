47189e95d79e043af316c61696561cf56f24a95f
Den Raskovalov <denplusplus@yandex-team.ru>
2021-07-11 19:39:09 -0700

    submit

--- solver_move/main.cpp
+++ solver_move/main.cpp
@@ -31,37 +31,45 @@ int main(int argc, char* argv[]) {
     for (int shiftX = -1000; shiftX < 1000; ++shiftX) {
         cerr << "..." << shiftX << endl;
         for (int shiftY = -1000; shiftY < 1000; ++shiftY) {
-            auto good = [&](int dx, int dy) {
-                for (size_t i = 0; i < p.originalPoints.size(); ++i) {
-                    Point np = p.originalPoints[i];
-                    np.x = np.x + dx;
-                    np.y = np.y + dy;
-                    auto toPoint = p.pointInsideToIndex.find(np);
-                    if (toPoint != p.pointInsideToIndex.end()) {
-                        sc.points[i] = toPoint->second;
-                    } else {
-                        // cerr << "No solution " << i << " (" << np.x << ", " << np.y << ")" << endl;
+            for (int trans = 0; trans < 2; ++trans) {
+                auto good = [&](int dx, int dy, bool trans) {
+                    for (size_t i = 0; i < p.originalPoints.size(); ++i) {
+                        const Point op = p.originalPoints[i];
+                        Point np = op;
+                        if (!trans) {
+                            np.x = op.x + dx;
+                            np.y = op.y + dy;
+                        } else {
+                            np.y = op.x + dx;
+                            np.x = op.y + dy;
+                        }
+                        auto toPoint = p.pointInsideToIndex.find(np);
+                        if (toPoint != p.pointInsideToIndex.end()) {
+                            sc.points[i] = toPoint->second;
+                        } else {
+                            // cerr << "No solution " << i << " (" << np.x << ", " << np.y << ")" << endl;
+                            return false;
+                        }
+                    }
+
+                    if (p.violationsBnd(sc)) {
                         return false;
                     }
+
+                    if (p.violationsLen(sc)) {
+                        return false;
+                    }
+
+                    cerr << "found " << dx << " " << dy << endl;
+                    std::ofstream f("../solutions/move/" + std::to_string(FLAGS_test_idx) + ".json");
+                    f << p.exportSol(sc.points);
+
+                    return true;
+                };
+
+                if (good(shiftX, shiftY, trans)) {
+                    return 0;
                 }
-
-                if (p.violationsBnd(sc)) {
-                    return false;
-                }
-
-                if (p.violationsLen(sc)) {
-                    return false;
-                }
-
-                cerr << "found " << dx << " " << dy << endl;
-                std::ofstream f("../solutions/move/" + std::to_string(FLAGS_test_idx) + ".json");
-                f << p.exportSol(sc.points);
-
-                return true;
-            };
-
-            if (good(shiftX, shiftY)) {
-                return 0;
             }
         }
     }
