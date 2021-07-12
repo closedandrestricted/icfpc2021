911eaaca3e992360f4d8d8061c0adf2e64c4cc5e
Evgeniy Krasko <evgeniykrasko@MacBook-Air.local>
2021-07-12 02:13:05 -0700

    new goldens

--- solver/main.cc
+++ solver/main.cc
@@ -89,6 +89,7 @@ int main(int argc, char** argv) {
             std::ofstream f("solutions/staging/" + std::to_string(FLAGS_test_idx) + ".json");
             f << p.exportSol(z.current.points);
         }
+        std::cout << "INITIALIZED" << std::endl;
         // exit(0);
 
         SolutionCandidate sol0 = z.current;
