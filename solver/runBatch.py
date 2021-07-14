9dcc2b56e2ab5803fdaa954d4fc915ed3936384f
Den Raskovalov <denplusplus@yandex-team.ru>
2021-07-13 19:00:03 -0700

    improve solutions

--- solver/runBatch.py
+++ solver/runBatch.py
@@ -21,11 +21,13 @@ parser.add_argument('--mcmc', action="store_true",
                     help='run MCMC solver')
 parser.add_argument('--timeout', type=int, default=60,
                     help='run timeout')
+parser.add_argument('--init', action="store_true",
+                    help='init')
 
 args = parser.parse_args()
 
 for problem in range(args.begin, args.end + 1, args.step):
     try:
-        subprocess.check_output(["./bazel-bin/solver/cli", "-test-idx", str(problem), "-alex" if not args.mcmc else ""], timeout=args.timeout)
+        subprocess.check_output(["./bazel-bin/solver/cli", "-test-idx", str(problem), "-alex" if not args.mcmc else ""] + (["-init", "solutions/golden/%d.json" % problem] if args.init else []), timeout=args.timeout)
     except:
         pass
